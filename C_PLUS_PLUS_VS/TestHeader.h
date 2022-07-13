8f13aab9bef9a37a086a2f46f5eeca79b325bb9a
SunYoung-P <p2sy45@gmail.com>
2022-07-14 01:18:08 +0900

    Test header file, sub source file

---
+++ C_PLUS_PLUS_VS/TestHeader.h
@@ -0,0 +1,1 @@
+double SqurtFunc(double x);
--- C_PLUS_PLUS_VS/main.cpp
+++ C_PLUS_PLUS_VS/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "TestHeader.h"
 
 int main()
 {
 	std::cout << "Hello World ! " << std::endl;
 	std::cout << "This is new environment ! " << std::endl;
+	std::cout << "5 suqrt is " << SqurtFunc(5) << std::endl
+		<< "19 squrt is " << SqurtFunc(19) << std::endl;
+
 	getchar();
 
 	return 0;
