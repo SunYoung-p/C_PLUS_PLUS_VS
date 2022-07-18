90fd12df4d57e9168ca33959c1f60ffb71f20c58
SunYoung-P <p2sy45@gmail.com>
2022-07-19 00:11:29 +0900

    0719 가위바위보 게임 커밋

--- C_PLUS_PLUS_VS/main.cpp
+++ C_PLUS_PLUS_VS/main.cpp
@@ -5,20 +5,6 @@ using std::cin;
 
 int main()
 {
-	int n = 0;
-
-	cout << "숫자를 입력하세요 : ";
-	cin >> n;
-
-	for (int i = n; i <= n+9; i++)
-	{
-		for (int j = 1; j <= 9; j++)
-		{
-			cout << i << " * " << j << " = " << i*j << std::endl;
-		}
-
-		cout << std::endl;
-	}
 
 	return 0;
 }
